bff955b3c7c2269d007b69b192dea7d624b93524
ataher-ali <ataher.dev@gmail.com>
2024-01-30 09:20:10 +0600

    home barnar

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,7 +3,12 @@
 const Home = () => {
     return (
         <div>
-            <img src="https://i.ibb.co/Ltk5Hxr/1.png" alt="" />
+            <div className="">
+            <img className="mx-auto block" src="https://i.ibb.co/Ltk5Hxr/1.png" alt="" />
+            
+            <h2 className="text-2xl text-center my-5">১০ই ফেব্রয়ারি ২০২৪ থেকে শুরু </h2>
+            
+            </div>
             
         </div>
     );
