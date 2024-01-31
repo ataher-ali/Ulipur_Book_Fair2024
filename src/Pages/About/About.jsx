5d19d0a036d9071f60f0c0d0d050ff061fd7888e
ataher-ali <ataher.dev@gmail.com>
2024-01-31 23:33:35 +0600

    tasmia data

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -2,7 +2,16 @@
 const About = () => {
     return (
         <div>
-            About
+            <div className="mx-2 md:mx-10 lg:mx-36">
+                <h2 className="text-center text-3xl font-bold my-4"> About </h2>
+
+                <div className="my-2">
+                    <p className="text-justify">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati vero minus sunt, hic saepe consectetur. Fugit veniam vel, modi consequatur ut cupiditate error repudiandae ea illo, suscipit temporibus cumque tempore accusamus consequuntur vero eum doloremque est? Eum ex praesentium placeat, cum et asperiores, blanditiis, autem nihil nam eligendi perspiciatis! Vitae culpa ad fugiat eum, ullam soluta pariatur ut maiores fuga vero! Pariatur nemo unde labore, natus tenetur id alias fuga ipsam, ut commodi dolorum nam recusandae? Perspiciatis quod tempore ad laudantium ratione hic dolorum reiciendis veniam soluta, voluptatibus suscipit voluptate, animi molestiae adipisci nobis cumque omnis libero dolore ipsum voluptas?
+                    </p>
+
+                </div>
+            </div>
         </div>
     );
 };
