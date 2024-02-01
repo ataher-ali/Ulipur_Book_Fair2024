06564810d23ee66d0f987ea746c796256bafc9d6
ataher-ali <ataher.dev@gmail.com>
2024-02-01 17:46:11 +0600

    about

--- src/Pages/Arcive/Arcive Card/ArciveCard.jsx
+++ src/Pages/Arcive/Arcive Card/ArciveCard.jsx
@@ -1,18 +1,31 @@
+import { Link } from "react-router-dom";
 
 const ArciveCard = () => {
     return (
-        <div>
+        <>
+        
+        <Link to={`/`}>
+        
+            <div className="card m-2 border w-96 bg-base-100 shadow-xl">
+
+
             <div className="card w-96 bg-base-100 shadow-xl">
-                <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
-                    </div>
+                <figure className="px-10 pt-10">
+                    <img src="https://i.ibb.co/prkzn7F/20240201-170052-0000.png" alt="Shoes" className="rounded-xl" />
+                </figure>
+                <div className="card-body items-center text-center">
+                    <h2 className="card-title">২৮তম উলিপুর বই মেলা</h2>
+                    <p>২০২৪</p>
+                    <div className="card-actions">
+                    <button className="btn  btn-outline bg-gray-100 text-black "> বিস্তারিত </button>
+                   
+                    
                 </div>
             </div>
         </div>
+        </div>
+        </Link>
+        </>
     );
 };
 
--- src/Pages/Arcive/Arcive.jsx
+++ src/Pages/Arcive/Arcive.jsx
@@ -1,9 +1,10 @@
+import ArciveCard from "./Arcive Card/ArciveCard";
 
 const Arcive = () => {
     return (
         <div className="mx-2 md:mx-10 lg:mx-36">
             <h2 className="text-center text-3xl font-bold my-4 mt-7 text-slate-600"> আর্কাইভ </h2>
-            
+            <ArciveCard></ArciveCard>
         </div>
     );
 };
